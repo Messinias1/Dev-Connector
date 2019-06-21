691177bf9dff750a4c87db7f2deccbff705d8b8d
Carl Kakisis <Messinias1@gmail.com>
2019-06-21 01:43:12 -0400

    created route files

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ connectDB();
 
 app.get("/", (req, res) => res.send("API Running"));
 
+// Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+
 const PORT = process.env.port || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
