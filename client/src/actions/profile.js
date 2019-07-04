b89c4c53cbf91b4504e6ac9f0704dd74ee7b7c3b
Carl Kakisis <Messinias1@gmail.com>
2019-07-04 20:04:26 +0200

    45 done

---
+++ client/src/actions/profile.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { sertAlert } from "./alert";
+
+import { GET_PROFILE, PROFILE_ERROR } from "./types";
+
+// Get current users profile
+export const getCurrentProfile = () => async dispatch => {
+  try {
+    const res = await axios.get("/api/profile/me");
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
