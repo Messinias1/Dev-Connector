810a7951661e2b6fcc48f058441ecb95a07cde20
Carl Kakisis <Messinias1@gmail.com>
2019-07-09 16:17:51 +0200

    59 done

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -64,7 +64,7 @@ export const getProfileById = userId => async dispatch => {
 };
 
 // Get github Repos
-export const getGithubRepo = username => async dispatch => {
+export const getGithubRepos = username => async dispatch => {
   try {
     const res = await axios.get(`/api/profile/github/${username}`);
 
